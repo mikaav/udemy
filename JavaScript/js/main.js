a6dc1f360c9fd7483d01913984810d477e44c15e
Karina Kantuni <avagyan_mika@mail.ru>
2020-03-27 15:57:40 +0400

    update js

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -7,3 +7,15 @@ alert(
     (148 - tweetCount) +
     " characters remaining."
 );
+
+var name = prompt("What is your name?");
+// first = name.slice(0, 1).toUpperCase();
+// second = name.slice(1).toLowerCase();
+// alert("Hello " + first + second);
+alert("Hello " + name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase());
+
+// Dog's age to Human's age
+
+var dogAge = prompt("Enter your dog's age:");
+var dogToHuman = (dogAge - 2) * 4 + 21;
+alert("If your dog will be a Human his age will be: " + dogToHuman + " years");
