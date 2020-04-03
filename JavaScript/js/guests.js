cfaf4f054b480c249327f4989001240788696af5
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-03 16:15:54 +0400

    adding fibonacci code

--- JavaScript/js/guests.js
+++ JavaScript/js/guests.js
@@ -34,3 +34,37 @@ function whosPaying(names) {
   var randomPerson = names[randomPersonPosition];
   return randomPerson + " is going to buy lunch today!";
 }
+
+//---------------------------------------------------------------------------------------------------
+
+var numberOfBottles = 99;
+while (numberOfBottles >= 0) {
+  var bottleWord = "bottle";
+  if (numberOfBottles === 1) {
+    bottleWord = "bottles";
+  }
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+  console.log(numberOfBottles + " " + bottleWord + " of beer,");
+  console.log("Take one down, pass it around,");
+  numberOfBottles--;
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+
+//------------------------------------------------------------------------------------------------------
+
+function fibonacciGenerator(n) {
+  var output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    return output[(0, 1)];
+  } else {
+    output = [0, 1];
+
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+
+  return output;
+}
