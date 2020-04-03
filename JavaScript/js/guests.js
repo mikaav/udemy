7f82d462be54a305fe04c185de53ff0cd6c192df
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-03 15:19:13 +0400

    update js

--- JavaScript/js/guests.js
+++ JavaScript/js/guests.js
@@ -6,3 +6,31 @@ if (guestList.includes(guestName)) {
 } else {
   alert("May be next time!");
 }
+
+//--------------------------------------------------------------------------------------------------
+
+var output = [];
+var count = 1;
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+
+  count++;
+  console.log(output);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+function whosPaying(names) {
+  var numberOfPeople = names.lenght;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+  return randomPerson + " is going to buy lunch today!";
+}
