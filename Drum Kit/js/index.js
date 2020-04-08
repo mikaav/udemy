916982c0394515313eda96ddf44e8c61c3f41ba9
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-08 14:13:42 +0400

    adding animation

--- Drum Kit/js/index.js
+++ Drum Kit/js/index.js
@@ -9,12 +9,14 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 function handleClick() {
   var buttonInnerHTML = this.innerHTML;
   makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
 }
 
 // Detecting Keyboard Press
 
 document.addEventListener("keypress", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -57,4 +59,12 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
