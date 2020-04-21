81e27f00046de8209d1ae5c7f5a254c2178fde64
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-21 17:16:28 +0400

    Update

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,19 @@
 
 const express = require("express");
 const app = express();
+
+app.get("/", function (request, response) {
+  response.send("Hello User");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Call Me or send me Email");
+});
+
+app.get("/about", function (req, res) {
+  res.send("I am localhost server working on NodeJs");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
