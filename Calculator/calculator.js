249bddcbb5f36e59c26662cfee7c9b3aaf1e579a
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-22 14:38:26 +0400

    add BMI calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,6 +5,8 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// for index.html (Calculator)
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
@@ -18,6 +20,22 @@ app.post("/", function (request, response) {
 
   // response.send("Thanks for using our app");
 });
+
+// for bmiCalculator.html  (BMI Calculator)
+
+app.get("/bmicalculator", function (request, response) {
+  response.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (request, response) {
+  var w1 = Number(req.body.val1);
+  var h1 = Number(req.body.val2);
+  var result1 = w1 * h1;
+
+  response.send("Your BMI is " + result1);
+});
+
+//for runnind server
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
