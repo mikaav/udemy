6febfc1345b7d01c8f1623b8506c34b583b56cf1
Karina Kantuni <avagyan_mika@mail.ru>
2020-04-23 12:47:17 +0400

    final version of calculator and bmi alculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -15,7 +15,6 @@ app.post("/", function (request, response) {
   var num1 = Number(request.body.n1);
   var num2 = Number(request.body.n2);
   var result = num1 + num2;
-  console.log("num1");
 
   response.send("The result of the calculation is:" + result);
 
