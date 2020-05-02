0c317acda2f32287de8f158f7ba7938284d097b3
Karina Kantuni <avagyan_mika@mail.ru>
2020-05-02 11:29:25 +0400

    Working with newsletter subscribe

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,12 +3,57 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
-const https = require("https");
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/8360c002b1";
+
+  const options = {
+    method: "POST",
+    auth: "mikayel92:9659e8dcd1f377834c644f1956877839-us8",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
+
+// API key mailchimp.com
+//  9659e8dcd1f377834c644f1956877839-us8
+
+// List ID
+//  8360c002b1
